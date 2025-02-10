672ae32b7f51d7c730b7a487e24294246000b7db
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2025-02-10 13:34:09 +0100

    translations treatments page

--- src/pages/Treatments.jsx
+++ src/pages/Treatments.jsx
@@ -1,5 +1,6 @@
 import "../css/Treatments.css";
 import box from "../assets/box-2.png";
+import { t } from "../utils/transllationFunction.js";
 
 function Treatments() {
   return (
@@ -7,94 +8,59 @@ function Treatments() {
       <header>
         <img src={box} alt="box" />
         <div className="intro-text-section">
-          <span id="odoimplant-title">Tractaments</span>
+          <span id="odoimplant-title">{t("tractaments")}</span>
         </div>
       </header>
 
-      <p className="ortodoncia-text">
-        A odoimplant tots els tractaments comencen amb un assessorament
-        personalitzat i complet. A la primera visita els doctors avaluen i
-        planifiquen el diagnòstic de cada pacient a mida. Som conscients que el
-        més important a la salut bucal és una bona prevenció i adquisició
-        d’hàbits bucodentals, és per això que tenim un equip amb especialitats
-        per tractar i prevenir totes les àrees. Som una clínica dental amb tota
-        mena d’especialitats i cobrim tots els tractaments al centre.
-      </p>
+      <p className="ortodoncia-text">{t("tractaments-text")}</p>
       <section className="ortodoncia-section treatments-list">
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Estètica </span> dental
+            <span>{t("estetica")} </span>
           </div>
-          <p>
-            La estètica dental és l’especialitat encarregada de tractar els
-            problemes relacionats amb la arminia bucal, tenint com a objectiu
-            aconseguir el somriure ideal.
-          </p>
+          <p>{t("estetica-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Pròtesi </span> dental
+            <span>{t("protesi")} </span>
           </div>
-          <p>Especialitat encarregada de reemplaçar les absències dentals.</p>
+          <p>{t("protesi-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Odonto</span>pediatria
+            <span>{t("odontopediatra")}</span>
           </div>
-          <p>
-            Especialitat encarregada de tractar la cura oral, preventiu i
-            terapèutic dels més petits de la família.
-          </p>
+          <p>{t("odontopediatra-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Endodòncia </span>
+            <span>{t("endodoncia")} </span>
           </div>
-          <p>
-            L’endodòncia, o tractament de conducte, és el procediment que es
-            realitza per eliminar, parcialment o totalment, la polpa (el nervi)
-            de la dent; i emplenar el conducte de l’arrel amb l’objectiu
-            principal de conservar la dent o el queixal natural. Les causes més
-            freqüents per les quals es requereix una endodòncia són: càries i/o
-            traumatismes, entre d’altres.
-          </p>
+          <p>{t("endodoncia-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Odontologia </span>general
+            <span>{t("odonto-gen")} </span>
           </div>
-          <p>Especialitat encarregada de diagnosticar els problemes bucals.</p>
+          <p>{t("odonto-gen-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Radiologia</span>
+            <span>{t("radiologia")}</span>
           </div>
-          <p>
-            Les radiografies dentals són molt importants en el diagnòstic
-            d’anomalies que afecten les dents i els ossos de suport que no podem
-            detectar en una exploració clínica visual.
-          </p>
+          <p>{t("radiologia-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Cirurgia</span> i implants
+            <span>{t("cirurgia")}</span>
           </div>
-          <p>
-            La cirurgia d’implants dentals és un procediment que reemplaça les
-            arrels de les dents amb pals metàl·lics similars a caragols i
-            reemplaça les dents danyades o que manca amb dents artificials que
-            es veuen i funcionen de manera molt similar als reals.
-          </p>
+          <p>{t("cirurgia-text")}</p>
         </div>
         <div className="ortodoncia-text treatments">
           <div className="title">
-            <span>Periodòncia</span>
+            <span>{t("periodoncia")}</span>
           </div>
-          <p>
-            La periodòncia és una branca de l’odontologia que s’especialitza en
-            la prevenció, el diagnòstic i el tractament de la malaltia
-            periodontal (malaltia de l’estructura que envolta la dent).
-          </p>
+          <p>{t("periodoncia-text")}</p>
         </div>
       </section>
     </>
