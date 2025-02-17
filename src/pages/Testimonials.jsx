6e03413daef1ce4ce287d21a8ca6b4e8b693dde4
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2025-02-17 11:09:25 +0100

    fix videos on testimonials page

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import LazyVideo from "../components/LazyVideo.jsx";
 import "../css/testimonials.css";
+import testimoni1 from "../assets/testimoni1.mp4";
+import testimoni2 from "../assets/testimoni2.mp4";
+import testimoni3 from "../assets/testimoni3.mp4";
+import testimoni4 from "../assets/testimoni4.mp4";
+import testimoni5 from "../assets/testimoni5.mp4";
 
 function Testimonials() {
   return (
     <section id="video-container" class="bg-gray-100 ">
       <div className="video">
-        <LazyVideo videoSrc="src/assets/testimoni1.mp4" thumbnail="" />
+        <LazyVideo videoSrc={testimoni1} thumbnail="" />
       </div>
       <div className="video">
-        <LazyVideo videoSrc="src/assets/testimoni2.mp4" thumbnail="" />
+        <LazyVideo videoSrc={testimoni2} thumbnail="" />
       </div>
       <div className="video">
-        <LazyVideo videoSrc="src/assets/testimoni3.mp4" thumbnail="" />
+        <LazyVideo videoSrc={testimoni3} thumbnail="" />
       </div>
       <div className="video">
-        <LazyVideo videoSrc="src/assets/testimoni4.mp4" thumbnail="" />
+        <LazyVideo videoSrc={testimoni4} thumbnail="" />
       </div>
       <div className="video">
-        <LazyVideo videoSrc="src/assets/testimoni5.mp4" thumbnail="" />
+        <LazyVideo videoSrc={testimoni5} thumbnail="" />
       </div>
     </section>
   );
