1694e8c2a0d9b99c941901df37ab3a82554c26d5
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2025-03-28 21:28:30 +0100

    videos nous

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -7,28 +7,40 @@ import testimoni3 from "../assets/testimoni3.mp4";
 import testimoni4 from "../assets/testimoni4.mp4";
 import testimoni5 from "../assets/testimoni5.mp4";
 import testimoni6 from "../assets/testimoni6.mp4";
+import testimoni7 from "../assets/testimoni7.mp4";
+import testimoni8 from "../assets/testimoni8.mp4";
 
 function Testimonials() {
   return (
     <section id="video-container" class="bg-gray-100 ">
-      <div className="video">
-        <LazyVideo videoSrc={testimoni1} thumbnail="" />
-      </div>
-      <div className="video">
-        <LazyVideo videoSrc={testimoni2} thumbnail="" />
-      </div>
-      <div className="video">
-        <LazyVideo videoSrc={testimoni3} thumbnail="" />
-      </div>
-      <div className="video">
-        <LazyVideo videoSrc={testimoni4} thumbnail="" />
-      </div>
-      <div className="video">
-        <LazyVideo videoSrc={testimoni5} thumbnail="" />
-      </div>
-      <div className="video">
-        <LazyVideo videoSrc={testimoni6} thumbnail="" />
-      </div>
+      <section className="vid-sect">
+        <div className="video">
+          <LazyVideo videoSrc={testimoni1} thumbnail="" />
+        </div>
+        <div className="video">
+          <LazyVideo videoSrc={testimoni2} thumbnail="" />
+        </div>
+        <div className="video">
+          <LazyVideo videoSrc={testimoni3} thumbnail="" />
+        </div>
+        <div className="video">
+          <LazyVideo videoSrc={testimoni4} thumbnail="" />
+        </div>
+      </section>
+      <section className="vid-sect">
+        <div className="video">
+          <LazyVideo videoSrc={testimoni5} thumbnail="" />
+        </div>
+        <div className="video">
+          <LazyVideo videoSrc={testimoni6} thumbnail="" />
+        </div>
+        <div className="video">
+          <LazyVideo videoSrc={testimoni7} thumbnail="" />
+        </div>
+        <div className="video">
+          <LazyVideo videoSrc={testimoni8} thumbnail="" />
+        </div>
+      </section>
     </section>
   );
 }
