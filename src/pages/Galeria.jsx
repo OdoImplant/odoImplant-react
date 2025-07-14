f9410c519b6a2fcd66a32ea1c6ae0ece9d125d22
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2025-07-14 21:55:18 +0200

    galery update

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -32,50 +32,44 @@ import gal26 from "../assets/gal-26.png";
 
 function Galeria() {
   return (
-    <div className="galery-top">
-      <Carousel className="rounded-l">
-        <img
-          src={lupas}
-          alt="doctora 1"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src={alineadores}
-          alt="alineadores"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src={doctora}
-          alt="doctora 2"
-          className="h-full w-full object-cover"
-        />
-        <img src={gal1} alt="" className="h-full w-full object-cover" />
-        <img src={gal2} alt="" className="h-full w-full object-cover" />
-        <img src={gal3} alt="" className="h-full w-full object-cover" />
-        <img src={gal4} alt="" className="h-full w-full object-cover" />
-        <img src={gal5} alt="" className="h-full w-full object-cover" />
-        <img src={gal6} alt="" className="h-full w-full object-cover" />
-        <img src={gal7} alt="" className="h-full w-full object-cover" />
-        <img src={gal8} alt="" className="h-full w-full object-cover" />
-        <img src={gal9} alt="" className="h-full w-full object-cover" />
-        <img src={gal10} alt="" className="h-full w-full object-cover" />
-        <img src={gal11} alt="" className="h-full w-full object-cover" />
-        <img src={gal12} alt="" className="h-full w-full object-cover" />
-        <img src={gal13} alt="" className="h-full w-full object-cover" />
-        <img src={gal14} alt="" className="h-full w-full object-cover" />
-        <img src={gal15} alt="" className="h-full w-full object-cover" />
-        <img src={gal16} alt="" className="h-full w-full object-cover" />
-        <img src={gal17} alt="" className="h-full w-full object-cover" />
-        <img src={gal18} alt="" className="h-full w-full object-cover" />
-        <img src={gal19} alt="" className="h-full w-full object-cover" />
-        <img src={gal20} alt="" className="h-full w-full object-cover" />
-        <img src={gal21} alt="" className="h-full w-full object-cover" />
-        <img src={gal22} alt="" className="h-full w-full object-cover" />
-        <img src={gal23} alt="" className="h-full w-full object-cover" />
-        <img src={gal24} alt="" className="h-full w-full object-cover" />
-        <img src={gal25} alt="" className="h-full w-full object-cover" />
-        <img src={gal26} alt="" className="h-full w-full object-cover" />
-      </Carousel>
+    <div className="galery-img">
+      <img src={lupas} alt="doctora 1" className="h-full w-full object-cover" />
+      <img
+        src={alineadores}
+        alt="alineadores"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src={doctora}
+        alt="doctora 2"
+        className="h-full w-full object-cover"
+      />
+      <img src={gal1} alt="" className="h-full w-full object-cover" />
+      <img src={gal2} alt="" className="h-full w-full object-cover" />
+      <img src={gal3} alt="" className="h-full w-full object-cover" />
+      <img src={gal4} alt="" className="h-full w-full object-cover" />
+      <img src={gal5} alt="" className="h-full w-full object-cover" />
+      <img src={gal6} alt="" className="h-full w-full object-cover" />
+      <img src={gal7} alt="" className="h-full w-full object-cover" />
+      <img src={gal8} alt="" className="h-full w-full object-cover" />
+      <img src={gal9} alt="" className="h-full w-full object-cover" />
+      <img src={gal10} alt="" className="h-full w-full object-cover" />
+      <img src={gal11} alt="" className="h-full w-full object-cover" />
+      <img src={gal12} alt="" className="h-full w-full object-cover" />
+      <img src={gal13} alt="" className="h-full w-full object-cover" />
+      <img src={gal14} alt="" className="h-full w-full object-cover" />
+      <img src={gal15} alt="" className="h-full w-full object-cover" />
+      <img src={gal16} alt="" className="h-full w-full object-cover" />
+      <img src={gal17} alt="" className="h-full w-full object-cover" />
+      <img src={gal18} alt="" className="h-full w-full object-cover" />
+      <img src={gal19} alt="" className="h-full w-full object-cover" />
+      <img src={gal20} alt="" className="h-full w-full object-cover" />
+      <img src={gal21} alt="" className="h-full w-full object-cover" />
+      <img src={gal22} alt="" className="h-full w-full object-cover" />
+      <img src={gal23} alt="" className="h-full w-full object-cover" />
+      <img src={gal24} alt="" className="h-full w-full object-cover" />
+      <img src={gal25} alt="" className="h-full w-full object-cover" />
+      <img src={gal26} alt="" className="h-full w-full object-cover" />
     </div>
   );
 }
